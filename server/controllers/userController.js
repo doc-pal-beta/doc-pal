d2fc4c254db400aa4f108267b54a842ab8a2f18d
bryannadejesus <bryanna.dejesus@gmail.com>
2021-12-31 18:59:47 -0500

    added createDoc router and controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,8 +3,18 @@ const models = require("../models/userModels");
 
 const userController = {};
 
-userController.signUp = (req, res, next) => {
-  next()
-}
+userController.createDoc = (req, res, next) => {
+  const { firstName, lastName, password } = req.body;
+
+  const newDoc = new Doc({ firstName: firstName, lastName: lastName, password: password });
+
+  try {
+    const result = Doc.create(newDoc);
+    res.locals.doc = result;
+    return next();
+  } catch (error) {
+    res.sendStatus(400).json(error);
+  }
+};
 
 module.exports = userController;
--- server/server.js
+++ server/server.js
@@ -3,9 +3,15 @@ const app = express();
 const path = require('path');
 const userController = require("./controllers/userController");
 
+
 app.get('/', (req, res) => {
     res.status(200).send('Test');
 });
+
+app.get('/createDoc', userController.createDoc,  (req, res) => {
+    console.log("I made it to docCreate");
+    res.status(200).json(res.locals.doc);
+  });
 
 app.listen(3000, () => console.log('listening on port 3000')); //listens on port 3000 -> http://localhost:3000/
 
