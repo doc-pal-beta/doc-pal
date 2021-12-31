f4ec240090d9814f48e8e5e624253e39a24f06ac
Nader Almogazy <nader73107@yahoo.com>
2021-12-31 18:34:35 -0500

    set up first draft of patient, doctor, and doctor's visit Models

--- server/models/userModels.js
+++ server/models/userModels.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-const pass = require('./pass.json')
-const MONGO_URI = `mongodb+srv://${pass}@cluster0.ftr2x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+const pass = require("./pass.json");
+const MONGO_URI = `mongodb+srv://${pass}@cluster0.ftr2x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 mongoose
   .connect(MONGO_URI, {
@@ -12,12 +12,68 @@ mongoose
   .then(() => console.log("Connected to Mongo DB."))
   .catch((err) => console.log(err));
 
-  const Schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
+const patientSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  password: { type: String, required: true },
+  dateOfBirth: { type: String, required: true }, // MM/DD/YYYY
+  sex: String,
+  language: String,
+  address: String,
+  primaryDoctor: {
+    type: Schema.Types.ObjectId,
+    ref: "doctor",
+  },
+  visits: [
+    {
+      visitDate: { type: String, required: true }, // MM/DD/YYYY
+      id: {
+        type: Schema.Types.ObjectId,
+        ref: "visit",
+      },
+    },
+  ],
+});
 
+const doctorSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  password: { type: String, required: true },
+  licenseNumber: { type: Number, required: true },
+  title: String,
+  patients: [
+    {
+      lastName: { type: String, required: true },
+      dateOfBirth: { type: String, required: true }, // MM/DD/YYYY
+      id: {
+        type: Schema.Types.ObjectId,
+        ref: "patient",
+      },
+    },
+  ],
+});
 
+const visitSchema = new Schema({
+  date: { type: String, required: true }, // MM/DD/YYYY
+  //SOAP NOTE: S:subjective complaints, O:objective observations, A:assessment/diagnosis, P:plan
+  subjective: String,
+  objective: String,
+  assessment: String,
+  plan: String,
+  prescription: String,
+  homeCare: String,
+});
 
+const Visit = mongoose.model("visit", visitSchema);
 
+const Patient = mongoose.model("patient", patientSchema);
 
-  module.exports = {
-  }
+const Doctor = mongoose.model("doctor", doctorSchema);
+
+module.exports = {
+  Visit,
+  Patient,
+  Doctor,
+};
