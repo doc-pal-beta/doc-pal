2041feb4ac72a6ba36e1be6b2e2bd25a50dd3d9d
Nader Almogazy <nader73107@yahoo.com>
2021-12-31 21:20:36 -0500

    deleted extraneous consolelogs

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,13 +4,7 @@ const { Visit, Doctor, Patient } = models;
 const userController = {};
 
 userController.createDoctor = (req, res, next) => {
-  //const { firstName, lastName, password } = req.body;
-  console.log(req.body)
-  
     Doctor.create(req.body, (error, success) => {
-      console.log("hi")
-      console.log(error);
-      console.log(success);
       if (error) res.sendStatus(400).json(error);
       res.locals.doctor = success;
       return next();
