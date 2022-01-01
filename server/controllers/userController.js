13370743cca517c9b3786caf58ee8da55c5f34fd
Nader Almogazy <nader73107@yahoo.com>
2021-12-31 20:41:44 -0500

    added post functionality for patient and doctor

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,10 +1,38 @@
 const models = require("../models/userModels");
-// const { CharacterSheet, User } = models;
+const { Visit, Doctor, Patient } = models;
 
 const userController = {};
 
-userController.signUp = (req, res, next) => {
-  next()
+userController.createDoctor = (req, res, next) => {
+  //const { firstName, lastName, password } = req.body;
+  console.log(req.body)
+  
+    Doctor.create(req.body, (error, success) => {
+      console.log("hi")
+      console.log(error);
+      console.log(success);
+      if (error) res.sendStatus(400).json(error);
+      res.locals.doctor = success;
+      return next();
+    })
+};
+
+userController.createPatient = (req, res, next) => {
+  Patient.create(req.body, (error, success) => {
+    if (error) next(error);
+    res.locals.currentPatient = success;
+    next();
+  })
+}
+
+userController.getPatientInfo = (req, res, next) => {
+  // Expect to recieve query: /getPatientInfo/?lastName=<LASTNAME>&dateOfBirth=<MM/DD/YYYY>
+  const {lastName, dateOfBirth}  = req.query
+  Patient.findOne({lastName, dateOfBirth}, (error, success) => {
+    if (error) next(error);
+    res.locals.currentPatient = success;
+    next();
+  })
 }
 
 module.exports = userController;
