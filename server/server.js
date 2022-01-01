2edcc0b9732c9084ee498a68bf35f114832d740f
Nader Almogazy <nader73107@yahoo.com>
2022-01-01 15:37:28 -0500

    refactored all gets and posts.

--- server/server.js
+++ server/server.js
@@ -9,28 +9,45 @@ app.get("/", (req, res) => {
   res.status(200).send("Test");
 });
 
-app.get("/patient", userController.getPatientInfo, (req, res) => {
-  res.status(200).json(res.locals.currentPatient);
+//GET
+//get patients through query search or empty for all
+app.get("/patients", userController.getPatients, (req, res) => {
+  res.status(200).json(res.locals.patients);
 });
-
-app.get("/doctor", userController.getDoctorInfo, (req, res) => {
-  res.status(200).json(res.locals.currentDoctor);
+//get patient by id
+app.get("/patients/:id", userController.getPatient, (req, res) => {
+  res.status(200).json(res.locals.patient);
 });
-
-app.post("/doctor", userController.createDoctor, (req, res) => {
-  console.log("I made it to docCreate");
+//get doctors, all or by query
+app.get("/doctors", userController.getDoctors, (req, res) => {
+  res.status(200).json(res.locals.doctors);
+});
+//get doctor by id
+app.get("/doctors/:id", userController.getDoctor, (req, res) => {
   res.status(200).json(res.locals.doctor);
 });
-
-app.post("/patient", userController.createPatient, (req, res) => {
-  res.status(200).json(res.locals.currentPatient);
+app.get("/visits", userController.getVisits, (req, res) => {
+  res.status(200).json(res.locals.visits);
 });
 
-app.post("/visit", userController.createVisit, (req, res) => {
-  //Doctor creates OR selects a patient from his patient list
-  //This route expects all visit information AS WELL AS doctorID and patientId
-  res.status(200).json(res.locals.currentVisit);
+//POST
+app.post("/doctors", userController.createDoctor, (req, res) => {
+  res.status(200).json(res.locals.newDoctor);
 });
+app.post("/patients", userController.createPatient, (req, res) => {
+  res.status(200).json(res.locals.patients);
+});
+app.post(
+  "/visits",
+  userController.createVisit,
+  userController.linkVisitToPatient,
+  (req, res) => {
+    //Doctor creates OR selects a patient from his patient list
+    //This route expects all visit information AS WELL AS doctorID and patientId
+    //will automatically add visit to patients records.
+    res.status(200).json(res.locals.newVisit);
+  }
+);
 
 app.use("*", (req, res) => {
   res.status(404).send("Page not Found");
